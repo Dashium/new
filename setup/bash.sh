c39ffdf9558fd046d13d8e5b45241e6d6a69e0c3
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-05 16:38:09 +0200

    init v1.0

---
+++ setup/bash.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Mettre à jour le système
+sudo apt-get update && sudo apt-get upgrade -y
+
+# Vérifier si Node.js est installé
+if ! [ -x "$(command -v node)" ]; then
+  echo "Erreur : Node.js n'est pas installé sur le système."
+  exit 1
+fi
+
+# Installer Node.js
+sudo apt-get install nodejs -y
+
+# Vérifier si npm est installé
+if ! [ -x "$(command -v npm)" ]; then
+  echo "Erreur : npm n'est pas installé sur le système."
+  exit 1
+fi
+
+# Installer PM2
+sudo npm install pm2@latest -g
+
+# Vérifier si Git est installé
+if ! [ -x "$(command -v git)" ]; then
+  echo "Erreur : Git n'est pas installé sur le système."
+  exit 1
+fi
+
+# Cloner le dépôt Dashium
+sudo git clone https://github.com/Dashium/Dashium /dashium
+
+# Exécuter le script de configuration de Dashium
+cd /dashium
+sudo ./setup.sh
