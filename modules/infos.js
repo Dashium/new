2568a0a0d36cb687564a204d1f4aa62021b66d93
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-07 18:39:38 +0200

    Update txt starting

--- modules/infos.js
+++ modules/infos.js
@@ -1,4 +1,13 @@
+const config = require('../config/global.json');
 const version = require('../package.json');
+
+function printer(txt){
+    var n = txt.length;
+    var tmp = "                                           |";
+        tmp = tmp.slice(n, tmp.length);
+    console.log(`|    ${txt}${tmp}`);
+}
+
 console.log("|-----------------------------------------------|");
 console.log("|   _____            _     _                    |");
 console.log("|  |  __ \\          | |   (_)                   |");
@@ -7,10 +16,10 @@ console.log("|  | |  | |/ _\\` / __| '_ \\| | | | | '_ \` _ \\   |");
 console.log("|  | |__| | (_| \\__ \\ | | | | |_| | | | | | |   |");
 console.log("|  |_____/ \\__,_|___/_| |_|_|\\__,_|_| |_| |_|   |");
 console.log("|-----------------------------------------------|");
-console.log(`|    Version: ${version.version}                             |`);
-console.log("|    Installation finish !                      |");
-console.log("|    enjoy ;)                                   |");
+printer(`Server name: ${config.server.name}`);
+printer(`Version: ${version.version}`);
+printer(`Web app: http://${config.server.host}:${config.server.port}`);
+printer(`API: http://${config.api.host}:${config.api.port}/api`);
+console.log("|    ---------------------------------------    |");
 console.log("|    © Tai Studio 2021/2023                     |");
 console.log("|-----------------------------------------------|");
-console.log(" ");
-console.log("Starting ...");
