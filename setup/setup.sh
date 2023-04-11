68464fae6a88e79a7e84e30e1ea6dc626af2a317
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-11 17:06:28 +0200

    UPDATE: node

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,7 +4,7 @@
 sudo apt-get update && sudo apt-get upgrade -y
 
 # Installer Node.js
-curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo apt-get install npm -y
 
