36863b800a3bfb1974297c37f43feea5d5eff34a
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-12 18:31:01 +0200

    FIX: next

--- modules/dashboard/next.config.js
+++ modules/dashboard/next.config.js
@@ -3,13 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-// module.exports = nextConfig
-
-module.exports = {
-  reactStrictMode: true,
-  module: {
-    loaders: [
-      { test: /\.css$/, loader: "style-loader!css-loader" },
-    ]
-  }
-};
+module.exports = nextConfig
