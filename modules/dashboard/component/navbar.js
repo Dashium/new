0d69c7cf31bee67eaa85dd3ac0ab6a0e643829d0
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-12 11:29:20 +0200

    FIX: img error

--- modules/dashboard/component/navbar.js
+++ modules/dashboard/component/navbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useState, useEffect } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { BsFillGridFill, BsFileEarmarkCodeFill, BsGearFill, BsArrowRepeat, BsStopFill } from 'react-icons/bs';
