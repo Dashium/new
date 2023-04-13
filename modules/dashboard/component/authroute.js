d1e1f46e45174dd8971b92cab79a640c55f0690f
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-13 18:11:28 +0200

    ADD: auth

---
+++ modules/dashboard/component/authroute.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Cookies from 'js-cookie';
+
+const checkAuth = () => {
+    const token = Cookies.get('auth_token');
+    if (!token) {
+        throw new Error('No token found');
+    }
+
+    // TODO: Vérifier si le token est valide en le vérifiant avec le serveur
+
+    return true;
+};
+
+const AuthRoute = ({ children }) => {
+    const router = useRouter();
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+        try {
+            setIsAuthenticated(checkAuth());
+        } catch (error) {
+            router.push('/login');
+        }
+    }, []);
+
+    return isAuthenticated ? children : null;
+};
+
+export default AuthRoute;
