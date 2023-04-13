841cd7e869566b1df7424a79dd340759d6f27f7d
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-13 18:12:28 +0200

    ADD: popup

--- modules/dashboard/pages/_app.js
+++ modules/dashboard/pages/_app.js
@@ -3,6 +3,7 @@ import './css/navbar.css';
 import './css/terminal.css';
 import './css/sidebar.css';
 import './css/login.css';
+import './css/popup.css';
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
