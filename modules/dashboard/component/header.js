93224b175eb8d68243b1f1bb5c847c57634e8f36
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-14 00:37:39 +0200

    UPDATE: title

--- modules/dashboard/component/header.js
+++ modules/dashboard/component/header.js
@@ -3,11 +3,10 @@ import React from 'react';
 import Head from 'next/head'
 
 const Headers = ({ title }) => {
-    if(title == null){title = 'Tai Studio';}
     return (
         <div>
             <Head>
-                <title>Dashium | {title}</title>
+                <title>Dashium | {title ? title : 'Tai Studio'}</title>
                 <link rel="shortcut icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="/font/Heebo.css" />
             </Head>
