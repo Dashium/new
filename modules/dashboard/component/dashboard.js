924f249cf25c5a8945f8bac3428a2c70edb34d23
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-13 22:43:33 +0200

    FIX: monitor position

--- modules/dashboard/component/dashboard.js
+++ modules/dashboard/component/dashboard.js
@@ -16,13 +16,7 @@ const Dashboard = () => {
 
             {/* <Sidebar></Sidebar> */}
 
-            <div style={{
-                position: 'relative',
-                top: '2%',
-                left: '2%',
-                right: '2%',
-                width: '100%'
-            }}>
+            <div>
                 <Monitor containerName={'o7vZWjYh31'}></Monitor>
                 <TerminalSSH containerName={'o7vZWjYh31'}></TerminalSSH>
             </div>
