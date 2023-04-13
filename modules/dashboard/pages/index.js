bd5d01a43787065ce526bbf7355054b102854f5f
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-13 18:12:12 +0200

    UPDATE: dashboard

--- modules/dashboard/pages/index.js
+++ modules/dashboard/pages/index.js
@@ -1,59 +1,28 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
-const config = require('../../../config/global.json');
 
 import Headers from '../component/header';
 import Background from '../component/background';
-import Sidebar from '../component/sidebar';
-import Navbar from '../component/navbar';
 
-import Login from '../component/login';
-
-import Monitor from '../component/monitor';
-import TerminalSSH from '../component/terminal';
-
-const client = new ApolloClient({
-    uri: `http://localhost:5050/api`,
-    cache: new InMemoryCache(),
-});
+import Dashboard from '../component/dashboard';
 
 const App = () => {
     return (
-        <ApolloProvider client={client}>
-            <div className='app' style={{
-                position: 'absolute',
-                left: 0,
-                top: 0,
-                width: '100%',
-                height: '100%',
-                overflow: 'hidden',
+        <div className='app' style={{
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            width: '100%',
+            height: '100%',
+            overflow: 'hidden',
 
-                userSelect: 'none',
-                MozUserSelect: 'none',
-                WebkitUserSelect: 'none'
-            }}>
-                <Headers></Headers>
-                <Background />
-
-                <Login api={config.api}></Login>
-
-                {/* <Sidebar></Sidebar> */}
-
-                <div style={{
-                    position: 'relative',
-                    top: '2%',
-                    left: '2%',
-                    right: '2%',
-                    width: '100%'
-                }}>
-                    <Monitor containerName={'o7vZWjYh31'}></Monitor>
-                    <TerminalSSH containerName={'o7vZWjYh31'}></TerminalSSH>
-                </div>
-
-                <Navbar></Navbar>
-
-            </div>
-        </ApolloProvider>
+            userSelect: 'none',
+            MozUserSelect: 'none',
+            WebkitUserSelect: 'none'
+        }}>
+            <Headers></Headers>
+            <Background mode='color' />
+            <Dashboard></Dashboard>
+        </div>
     );
 };
 
