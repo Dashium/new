251bff6238fd7c23a1c69fde54fb9232bc82ff6a
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-15 12:35:02 +0200

    UPDATE: remove all containers

--- setup/uninstaller.js
+++ setup/uninstaller.js
@@ -1,7 +1,22 @@
 const common = require('../modules/common');
+const docker = require('../modules/ci/docker');
+const dbModule = require('../modules/bdd/main');
 
-common.rmfile('./config/global.json');
-common.rmdir('./build');
-common.rmdir('./clusters');
-common.rmdir('./logs');
-common.rmfile('./config/dashium.db')
+async function removeDocker(){
+    var db = await dbModule.loadDatabase('dashium');
+    var data = await dbModule.selectRows(db, 'projects', '*');
+    data.forEach(async (curr) => {
+        var project = JSON.parse(curr.docker);
+        await docker.deleteContainer(project.dockerID);
+    });
+
+    await dbModule.closeDatabase(db);
+
+    common.rmfile('./config/global.json');
+    common.rmdir('./build');
+    common.rmdir('./clusters');
+    common.rmdir('./logs');
+    common.rmfile('./config/dashium.db');
+}
+
+removeDocker();
