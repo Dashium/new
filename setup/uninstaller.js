64c7d6fe79648a2dc03a77d5e66d857eee70b3f8
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-15 10:32:42 +0200

    ADD: rm build

--- setup/uninstaller.js
+++ setup/uninstaller.js
@@ -1,6 +1,7 @@
 const common = require('../modules/common');
 
 common.rmfile('./config/global.json');
+common.rmdir('./build');
 common.rmdir('./clusters');
 common.rmdir('./logs');
 common.rmfile('./config/dashium.db')
