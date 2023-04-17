a8b1f654fbf04115e70aa2df707f63c45b58d5db
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-17 14:05:34 +0200

    FIX: cors

--- modules/api/main.js
+++ modules/api/main.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", `http://${common.global.server.host}`);
+    res.header("Access-Control-Allow-Origin", `http://${common.global.server.host}:${common.global.server.port}`);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Credentials", true);
