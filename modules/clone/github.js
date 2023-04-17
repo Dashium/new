50114c6738c98ca0bb2d44ae3e844b64c91cafa4
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-17 13:03:33 +0200

    ADD: get sha

--- modules/clone/github.js
+++ modules/clone/github.js
@@ -1,13 +1,25 @@
 const common = require('../common');
 const { simpleGit } = require('simple-git');
 
+const getLatestCommitSha = async (repo) => {
+    try {
+        const latestCommit = await simpleGit(repo).log(['-n', '1']);
+        const latestCommitSha = latestCommit.all[0].hash;
+        return latestCommitSha;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
 module.exports = {
-    gitClone: function(repo, dest){
-        if(repo == null){ common.error('no repo has been set', 'github'); return;}
-        if(dest == null){ common.error('no destination has been set', 'github'); return;}
+    gitClone: function (repo, dest) {
+        if (repo == null) { common.error('no repo has been set', 'github'); return; }
+        if (dest == null) { common.error('no destination has been set', 'github'); return; }
         return simpleGit().clone(`https://${process.env.GH_TOKEN}@github.com/${repo}`, dest);
     },
-    gitFetch: function(repo, dest){
+    gitFetch: function (repo, dest) {
         return simpleGit(dest).fetch();
-    }
+    },
+    getLatestCommitSha
 }
