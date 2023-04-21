7bd8bf1443b53c87b5204f0cdc04ac9f33180ffa
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-21 19:40:00 +0200

    ADD: global ports

--- modules/global/main.js
+++ modules/global/main.js
@@ -1,13 +1,72 @@
 const common = require('../common');
 const dbModule = require('../bdd/main');
 
-async function updateSHA(sha) {
+async function addPort(port) {
     try {
         var bdd = await dbModule.loadDatabase('dashium');
         var entries = await dbModule.selectRows(bdd, 'global', '*', 'id = 1');
             entries[0].json = JSON.parse(entries[0].json);
 
-            entries[0].json.update = sha;
+        if (typeof entries[0].json.ports != 'object') {
+            entries[0].json.ports = [];
+        }
+
+        entries[0].json.ports.push(port);
+
+        updateGlobal(entries[0]);
+
+        return { message: 'Update successful' };
+    } catch (error) {
+        common.error(error, 'global');
+        return { error: 'Internal server error' };
+    }
+}
+
+async function getPorts() {
+    try {
+        var bdd = await dbModule.loadDatabase('dashium');
+        var entries = await dbModule.selectRows(bdd, 'global', '*', 'id = 1');
+            entries[0].json = JSON.parse(entries[0].json);
+
+        if (typeof entries[0].json.ports != 'object') {
+            entries[0].json.ports = [];
+        }
+
+        return entries[0].json.ports;
+    } catch (error) {
+        common.error(error, 'global');
+        return { error: 'Internal server error' };
+    }
+}
+
+async function removePort(port) {
+    try {
+        var bdd = await dbModule.loadDatabase('dashium');
+        var entries = await dbModule.selectRows(bdd, 'global', '*', 'id = 1');
+            entries[0].json = JSON.parse(entries[0].json);
+
+        if (typeof entries[0].json.ports != 'object') {
+            entries[0].json.ports = [];
+        }
+        
+        entries[0].json.ports = await common.removeValueFromArray(entries[0].json.ports, port);
+
+        updateGlobal(entries[0]);
+
+        return { message: 'Update successful' };
+    } catch (error) {
+        common.error(error, 'global');
+        return { error: 'Internal server error' };
+    }
+}
+
+async function updateSHA(sha) {
+    try {
+        var bdd = await dbModule.loadDatabase('dashium');
+        var entries = await dbModule.selectRows(bdd, 'global', '*', 'id = 1');
+        entries[0].json = JSON.parse(entries[0].json);
+
+        entries[0].json.update = sha;
 
         updateGlobal(entries[0]);
 
@@ -21,7 +80,7 @@ async function updateSHA(sha) {
 async function updateGlobal(data) {
     var db = await dbModule.loadDatabase('dashium');
     const global = await dbModule.selectRows(db, 'global', '*', 'id = 1');
-    
+
     if (data.json != null) {
         data.json = JSON.stringify(data.json);
     }
@@ -32,5 +91,8 @@ async function updateGlobal(data) {
 }
 
 module.exports = {
+    addPort,
+    getPorts,
+    removePort,
     updateSHA
 }
