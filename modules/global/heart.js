f7bfacbc679d231ff78bf83ed0fadf802875b960
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-25 20:02:39 +0200

    FIX: no restarting on linux

--- modules/global/heart.js
+++ modules/global/heart.js
@@ -4,20 +4,20 @@ const global = require('./main');
 const common = require('../common');
 
 function startScript() {
-    common.log('Starting script...', '💖 heart', true);
+    common.log('Starting script...', '💖 heart');
 
     const process = exec('npm run build');
 
     process.stdout.on('data', (data) => {
-        common.log(data, '💖 heart', true);
+        common.log(data, '💖 heart');
     });
 
     process.stderr.on('data', (data) => {
-        common.error(data, '💖 heart', true);
+        common.error(data, '💖 heart');
     });
 
     process.on('close', (code) => {
-        common.log(`Script exited with code ${code}. Restarting...`, '💖 heart', true);
+        common.log(`Script exited with code ${code}. Restarting...`, '💖 heart');
         startScript();
     });
 
@@ -29,19 +29,26 @@ function restartServer(pid) {
         // Windows
         exec(`taskkill /PID ${pid} /T /F`, (err, stdout, stderr) => {
             if (err) {
-                common.error(err, '💖 heart', true);
+                common.error(err, '💖 heart');
                 return;
             }
-            common.log(`Server stopped with PID: ${pid}`, '💖 heart', true);
+            common.log(`Server stopped with PID: ${pid}`, '💖 heart');
         });
     } else {
         // Unix / Mac
         exec(`kill ${pid}`, (err, stdout, stderr) => {
             if (err) {
-                common.error(err, '💖 heart', true);
+                common.error(err, '💖 heart');
+                exec(`sudo systemctl restart dashium`, (err, stdout, stderr) => {
+                    if (err) {
+                        common.error(err, '💖 heart');
+                        return;
+                    }
+                    common.log(`Server stopped with systemCTL`, '💖 heart');
+                });
                 return;
             }
-            common.log(`Server stopped with PID: ${pid}`, '💖 heart', true);
+            common.log(`Server stopped with PID: ${pid}`, '💖 heart');
         });
     }
 }
