1df5318e774973b5422b977133169ec415bec166
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-26 11:21:45 +0200

    FIX: header title

--- modules/dashboard/component/header.js
+++ modules/dashboard/component/header.js
@@ -3,14 +3,13 @@ import React from 'react';
 import Head from 'next/head'
 
 const Headers = ({ title }) => {
+    if (typeof title != 'string') { title = 'Tai Studio'; }
     return (
-        <div>
-            <Head>
-                <title>Dashium | {title ? title : 'Tai Studio'}</title>
-                <link rel="shortcut icon" href="/favicon.ico" />
-                <link rel="stylesheet" href="/font/Heebo.css" />
-            </Head>
-        </div>
+        <Head>
+            <title>{`Dashium | ${title}`}</title>
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="stylesheet" href="/font/Heebo.css" />
+        </Head>
     );
 };
 
