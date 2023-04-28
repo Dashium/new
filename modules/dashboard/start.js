803226bcaa30f3c025c353fc315bc4763fa8481c
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-04-28 16:49:05 +0200

    FIX: dev mode

--- modules/dashboard/start.js
+++ modules/dashboard/start.js
@@ -3,16 +3,17 @@ const common = require('../common');
 const https = require('https');
 const ssl = require('../ssl/main');
 
-const dev = false;
-const app = next({ dev, dir: './build' });
+const args = process.argv.slice(2);
+const isDev = args.includes('--dev');
+const dev = isDev ? true : false;
+const dir = isDev ? './modules/dashboard' : './build';
+const app = next({ dev, dir: dir });
 const handle = app.getRequestHandler();
 
 const SSLfile = ssl.getSSL();
 
 app.prepare().then(() => {
     https.createServer(SSLfile, (req, res) => {
-        // Be sure to pass `true` as the second argument to `url.parse`.
-        // This tells it to parse the query portion of the URL.
         const parsedUrl = require('url').parse(req.url, true);
         const { pathname, query } = parsedUrl;
 
@@ -27,6 +28,10 @@ app.prepare().then(() => {
     })
         .listen(common.global.server.port, (err) => {
             if (err) throw err;
-            common.sucess(`Ready on https://${common.global.server.host}:${common.global.server.port}`, 'server');
+            if (isDev) {
+                common.sucess(`Ready on http://${common.global.server.host}:${common.global.server.port} [DEV MODE]`, 'server');
+            } else {
+                common.sucess(`Ready on https://${common.global.server.host}:${common.global.server.port}`, 'server');
+            }
         });
 });
